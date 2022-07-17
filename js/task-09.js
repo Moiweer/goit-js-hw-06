881cb77cf21a98c6c2f07e136b40ecbecfd063b7
Mariya Barska <moiweer@gmail.com>
2022-07-18 01:35:37 +0300

    task 9
    change background color and span = hexformat

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+let valueColor = document.querySelector(".color")
+
+// function changeColor() {
+//   body.style.backgroundColor = getRandomHexColor();
+//   valueColor.textContent = `${body.style.backgroundColor}`
+// }
+function changeColor() {
+  const newColor = getRandomHexColor()
+  body.style.backgroundColor = newColor;
+  valueColor.textContent = `${newColor}`;
 }
+
+function getRandomHexColor() {
+
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
+}
+const colorResult = button.addEventListener("click", changeColor);
