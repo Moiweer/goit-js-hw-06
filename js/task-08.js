5d84b9f6ed8d40f3ca3bb7905e7908ba05a80068
Mariya Barska <moiweer@gmail.com>
2022-07-18 00:42:43 +0300

    task8
    input email and password, збереження данних як об'єкт

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
+const email = document.querySelector("[name=email]");
+const password =document.querySelector("[name=password]")
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const formData = {
+        email: email.value,
+        password: password.value,
+    };
+    console.log(formData)
+    form.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
